5a8844069a5cd84c3d491beb7ca457a056117e0f
Ry Racherbaumer <ry@xmtp.com>
2024-06-18 14:09:38 -0700

    Update start route

--- src/app/start/route.tsx
+++ src/app/start/route.tsx
@@ -5,13 +5,20 @@ export async function GET(request: Request) {
   try {
     return new ImageResponse(
       (
-        <div tw="flex flex-col w-full h-full justify-center items-center p-10 bg-[url('/assets/title.png')] bg-center">
-          <div tw="flex flex-col mb-4 items-center text-5xl">
+        <div tw="flex flex-col w-full h-full justify-center items-center p-10 bg-white relative">
+          <div tw="flex flex-col absolute w-80 h-80 z-0 left-1/2 ml-[-120px] top-[140px]">
+            <img
+              tw="h-full object-fit"
+              src={`${process.env.NEXT_PUBLIC_BASE_URL}/assets/${template.title.image}`}
+            />
+          </div>
+          <div tw="flex flex-col mb-4 items-center text-5xl relative z-10">
             {template.title.title_text}
           </div>
-          <div tw="flex flex-col text-2xl font-bold tracking-tight text-gray-900 justify-center">
+          <div tw="flex flex-col text-2xl font-bold tracking-tight text-gray-900 justify-center relative z-10">
             {template.title.sub_text}
           </div>
+          
         </div>
       )
     );
